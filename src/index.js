507b77486bf33da9e4ad6b83dd024811c92e5d6e
id-edm <ivandubochit@gmail.com>
2024-12-24 18:33:06 +0400

    feat: ready task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = [];
+  const bracketsPair = {};
+
+  bracketsConfig.forEach(([open, close]) => {
+    openBrackets.push(open);
+    bracketsPair[close] = open;
+  });
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    let currSymbol = str[i];
+
+    if (openBrackets.includes(currSymbol)) {
+     
+      if (bracketsPair[currSymbol] === currSymbol && stack[stack.length - 1] === currSymbol) {
+        stack.pop();
+      } else {
+        stack.push(currSymbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topEl = stack[stack.length - 1];
+      if (bracketsPair[currSymbol] === topEl) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
